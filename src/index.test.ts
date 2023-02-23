d126e2aaf009e7fec99b2af7a38b5df14cfa72fe
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2023-02-23 13:23:25 -0300

    test(tryTm): creating tests for tryTm

---
+++ src/index.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, vi, expect } from "vitest";
+import { trytm } from ".";
+
+describe("tryTm", () => {
+   it("Should return promise value if the promise resolves", async () => {
+      const promiseFn = vi
+         .fn()
+         .mockImplementationOnce(async () =>
+            Promise.resolve({ hey: "Bedesqui" }),
+         );
+
+      const [data, error] = await trytm(promiseFn());
+
+      expect(data).toStrictEqual({ hey: "Bedesqui" });
+      expect(error).toBeNull();
+   });
+
+   it("Should return error if the promise rejects", async () => {
+      const promiseFn = vi
+         .fn()
+         .mockImplementationOnce(async () =>
+            Promise.reject(new Error("I'm a failure")),
+         );
+
+      const [data, error] = await trytm(promiseFn());
+
+      expect(data).toBeNull();
+      expect(error).toBeInstanceOf(Error);
+      expect((error as Error).message).toBe("I'm a failure");
+   });
+});
