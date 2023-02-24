8fb1d6120d608a396ada30ff08fec49c9fed84ec
Krishna Rowter <krishna.rowter@gmail.com>
2023-02-24 19:52:13 +0700

    fix: return types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // blatantly stolen from fireship at https://www.youtube.com/watch?v=ITogH7lJTyE
 
-export const trytm = async <T>(promise: Promise<T>) => {
+export const trytm = async <T>(promise: Promise<T>): Promise<[T, null] | [null, Error]> => {
    try {
       const data = await promise;
-      return [data, null] as const;
-   } catch (error) {
-      return [null, error] as const;
+      return [data, null];
+   } catch (throwable) {
+      if (throwable instanceof Error) return [null, throwable]
+      
+      throw throwable
    }
 };
