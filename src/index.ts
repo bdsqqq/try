bb4a65198a98a017e15e240096137a7a6650abc6
Didier Demoniere <didier.demoniere@ezylean.com>
2023-03-02 01:49:19 +0100

    support object style

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
 // blatantly stolen from fireship at https://www.youtube.com/watch?v=ITogH7lJTyE
 
+export class OK<T> extends Array<T | null> {
+   public readonly [0]: null = null;
+   public readonly [1]: T;
+   public error: null = null;
+   public data: T;
+
+   constructor(data: T) {
+      super(2);  
+      this[1] = data;
+      this.data = data;
+   }
+}
+
+export class ERR<E> extends Array<E | null> {
+   public readonly [0]: E;
+   public readonly [1]: null = null;
+   public data: null = null;
+   public error: E;
+
+   constructor(error: E) {
+      super(2);  
+      this[0] = error;
+      this.error = error;
+   }
+} 
+
 export const trytm = async <T>(
    promise: Promise<T>,
-): Promise<[T, null] | [null, Error]> => {
+): Promise<OK<T> | ERR<Error>> => {
    try {
       const data = await promise;
-      return [data, null];
+      return new OK(data);
    } catch (throwable) {
-      if (throwable instanceof Error) return [null, throwable];
+      if (throwable instanceof Error) return new ERR(throwable);
 
       throw throwable;
    }
